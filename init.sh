69a89f5c15dee98e7d586292db745bfe7dbce4fb
Kamal Kumar <kamal2k89@gmail.com>
2024-12-26 01:57:50 +0530

    init

--- init.sh
+++ init.sh
@@ -17,18 +17,24 @@ ansible-galaxy role install gantsign.oh-my-zsh
 ansible-playbook .playbooks/configure-ubuntu-wsl.yaml
 ansible-playbook .playbooks/install-oh-my-zsh.yaml
 
+mv .zshrc .zshrc.bak -f 2> /dev/null
+mv .bashrc .bashrc.bak -f 2> /dev/null
+mv .tmux.conf .tmux.conf.bak -f 2> /dev/null
+stow . --dotfiles --adopt
+
 cat > ~/.gitconfig << EOF
 [user]
 	email = $EMAIL
 	name = $FULLNAME
 [init]
 	defaultBranch = main
+[safe]
+    directory = /home/$USERNAME/dotfiles
 EOF
 
-rm .git -rf
-git init
-git add .
-git commit -m 'Initial commit'
+## Cleaning clutter
+rm ~/.config/nvim/.git -rf
+rm .git -rf && git init
 
 echo "Please consider giving me a star at https://github.com/aworkaround/dotfiles.git !!"
 echo "You can create your own dotfiles repo and push ~/dotfiles directory there for further customizations!"
